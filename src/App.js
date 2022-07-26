5051e0ae67c571d87fd3c4e4e3fdd6f07a5ae123
Sejo Basic <sejoob@gmail.com>
2022-07-26 16:23:28 -0700

    Implemented function to shuffle card images by random

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css';
 
 const cardImages = [
@@ -10,7 +11,20 @@ const cardImages = [
 ]
 
 function App() {
-  
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // Shuffle cards, duplicate images array
+  // For each item in the array add on id property
+  function shuffleCards () {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
   return (
     <div className='App'>
       <h1>Pirate Match</h1>
