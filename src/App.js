6491d3b38403be95f16aeaf4ebfefcf336494329
Sejo Basic <sejoob@gmail.com>
2022-07-26 19:11:53 -0700

    Impleminting function that compares two selected cards and matching them

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Card from './components/Card'
 
 const cardImages = [
-  { 'src': '/assets/anchor.png' },
-  { 'src': '/assets/chest.png' },
-  { 'src': '/assets/flag.png' },
-  { 'src': '/assets/map.png' },
-  { 'src': '/assets/rum.png' },
-  { 'src': '/assets/sword.png' },
+  { 'src': '/assets/anchor.png', matched: false },
+  { 'src': '/assets/chest.png', matched: false },
+  { 'src': '/assets/flag.png', matched: false },
+  { 'src': '/assets/map.png', matched: false },
+  { 'src': '/assets/rum.png', matched: false },
+  { 'src': '/assets/sword.png', matched: false },
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+  // When user clicks on first card update choiceOne state value to be that card
+  // When user clicks on second card update choiceTwo state value to be that card
+
+
 
   // Shuffle cards, duplicate images array
   // For each item in the array add on id property
@@ -28,13 +35,57 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // Handle a choice
+  // If choice is selected it has a value if not we haven't selected something
+  // If it doesn't have value update choiceOne if it does update choiceTwo
+  function handleChoice(card) {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card) 
+  }
+
+  function resetTurn() {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+  }
+
+  // Compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      // If choiceOne src equals choiceTwo src then we have a match
+      // Then update the cards state taking the prev cards state to update it
+      // Then return new array of cards using map
+      // Each time the function fires we return the object that we want to place inside the new array, changing the matched property to true
+      // Else return card object
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      }else {
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
 
   return (
     <div className='App'>
       <h1>Pirate Match</h1>
       <button onClick={shuffleCards}>New Game</button>
-      <Card cards={cards} />
+      <div className="card-grid">
+        {cards.map(card => (
+          <Card 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice} 
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './Card.css'
 
-function Card({ cards }) {
+function Card({ card, handleChoice }) {
   
-  const renderCards = cards.map((card) => {
-    return (
-      <div className='card' key={card.id}>
-        <div>
-          <img className='front' src={card.src} alt='pirate card front' />
-          <img
-            className='back'
-            src='/assets/background.png'
-            alt='pirate card back'
-          />
-        </div>
-      </div>
-    )
-  })
+  // Update choice state
+  function handleClick() {
+    handleChoice(card)
+  }
 
-  return <div className='card-grid'>{renderCards}</div>
+  return (
+    <div className='card'>
+      <div>
+        <img className='front' src={card.src} alt='pirate card front' />
+        <img
+          className='back'
+          src='/assets/background.png'
+          alt='pirate card back'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Card
